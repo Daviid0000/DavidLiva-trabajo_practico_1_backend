d27c257dc60e1049ea3a7a358b043c2ff1dc9347
David Liva <davidliva01@gmail.com>
2023-08-28 13:30:27 -0300

    Segundo commit - Se ha creado el archivo de arranque del servidor

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+  app.listen(PORT, () => {
+    console.log('Server on port', PORT);
+  });
